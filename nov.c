d00af6157b7d88559a001fd34f23b3c75406137b
godwinnova <35991688+godwinnova@users.noreply.github.com>
2018-02-02 16:31:49 +0530

    Create nov.c

---
+++ nov.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+char c;
+int lowervowel,uppervowel;
+
+printf("\n Enter a alphabet");
+scanf("%d",alphabet);
+
+
+if(lowervowel//uppervowel)
+printf("\n Enter the lowervowel");
+scanf("%d",lowervowel);
+
+else
+printf("\n Enter the uppervowel");
+scanf("%d",uppervowel);
+return0;
+}
