2949d06fb25801b88565f7ce857fa113a74bc77f
godwinnova <35991688+godwinnova@users.noreply.github.com>
2018-02-07 14:22:45 +0530

    Create nova4.c

---
+++ nova4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int s,u,r;
+    printf("enter a values:");
+    scanf("%d",&s,&u);
+    r=pow(s,u);
+    printf("%d",r);
+    return 0;
+}
