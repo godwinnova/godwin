c1195e990e48ceb9ccabbd4338a44d512280e605
godwinnova <35991688+godwinnova@users.noreply.github.com>
2018-02-09 15:41:19 +0530

    Create nova7.c

---
+++ nova7.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+int n,m,i,flag=0;
+	
+printf("enter a number");
+	
+scanf("%d",&n);
+
+m=n/2;
+	
+for(i=2;i<=m;i++)
+	
+{
+
+if(n%i==0)
+		
+{
+			
+printf("not a prime number %d",n);
+			
+flag=1;
+		
+}
+	
+}
+	
+if(flag==0)
+	
+{
+		
+printf("prime number %d",n);
+	
+}
+	
+	
+return 0;
+
+}
