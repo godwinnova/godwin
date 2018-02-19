df4f7a1a5d154600c948b301c909e73f12687f9a
godwinnova <35991688+godwinnova@users.noreply.github.com>
2018-02-19 13:19:10 +0530

    Create nova53.c

---
+++ nova53.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main(void) 
+{
+	int n;
+	printf("enter a number:");
+	scanf("%d",&n);
+	if(n>=1 && n<=10)
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+	return 0;
+}
